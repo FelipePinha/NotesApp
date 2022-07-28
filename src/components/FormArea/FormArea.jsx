a718ff44669b80608b7f7795a1865cfd4391073b
Felipe Pinha <felipe_pc7@hotmail.com>
2022-07-28 17:27:17 -0300

    add form validation

--- src/components/FormArea/FormArea.jsx
+++ src/components/FormArea/FormArea.jsx
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCheck, FaSlash} from 'react-icons/fa'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from "react-hook-form";
+import { InputError } from "../InputError/InputError";
 import './formArea.css'
 
 export const FormArea = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const handleContentChange = (e) => {
+        setContent(e.target.value)
+    }
+
+    const onSubmit = () => {
+        console.log('CHECK')
+    }
+
+    const onError = () => {
+        console.log(errors)
+    }
+
+    const validationSchema = yup.object({
+        title: yup.string().required(),
+        content: yup.string().required()
+    })
+
+    const { register, handleSubmit, formState: {errors} } = useForm({ resolver: yupResolver(validationSchema) })
+
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit(onSubmit, onError)}>
 
             <div className="inputContainer">
-                <label htmlFor="title">Título</label>
+                <label htmlFor="title">
+                    Título
+                    {errors.title && <InputError />}
+                </label>
                 <input 
                     type="text" 
                     id="title" 
                     name="title"
+                    onChange={handleTitleChange}
+                    {...register('title')}
                 />
             </div>
             <div className="inputContainer">
-                <label htmlFor="content">Conteúdo</label>
+                <label htmlFor="content">Conteúdo
+                    {errors.content && <InputError />}
+                </label>
                 <textarea 
                     type="text" 
                     id="content"
@@ -23,6 +61,8 @@ export const FormArea = () => {
                     rows={15}
                     cols={30}
                     maxLength={580}
+                    onChange={handleContentChange}
+                    {...register('content')}
                 />
             </div>
 
---
+++ src/components/InputError/InputError.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import './inputError.css'
+
+export const InputError = () => {
+    return <span className="error">(*Este campo é obrigatório)</span>
+}
