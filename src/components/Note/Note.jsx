bf87389ce4ba6f366907fcb5db9bbeea046dca31
Felipe Pinha <felipe_pc7@hotmail.com>
2022-07-31 19:49:31 -0300

    add basic highlight note function

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import './note.css'
 
 export const Note = ({ title, content }) => {
+    const [highlight, setHighlight] = useState(false)
+
+    const handleHighlight = () => {
+        setHighlight(!highlight)
+    }
+
     return(
-        <div className="noteContainer">
+        <div 
+            className={`noteContainer ${highlight ? 'active' : ''}`}
+            onClick={handleHighlight}
+            >
             <div className="note">
                 <h1 className="title">{title}</h1>
                 <hr />
